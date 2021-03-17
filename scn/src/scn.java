1469ae1a79f65c6332c81d6bd2f0f720dbf12c0b
data20 <data20@DESKTOP-6LBSFJP>
2021-03-17 11:42:42 +0900

    실습 3/17일 첫 번째

---
+++ scn/src/scn.java
@@ -0,0 +1,35 @@
+
+public class scn{
+	
+
+
+public static void main(String[] args) {
+
+	int a,b,c,d;
+	a= 200;
+	b= a+100;
+	c= a+b -100;
+	d =a+b+c;
+	
+	System.out.println("a, b, c, dÀÇ °ª="+ a + ", " + b +", " + c + ", "+d);
+		
+		a= b = c= d =100;
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+	
+
+		a += a+100;
+		System.out.println(a);
+		
+              
+		
+    
+ 
+ 
+ 
+      }
+
+}
