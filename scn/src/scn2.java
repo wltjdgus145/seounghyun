4e38cf3d34fd09019f831909b6819152d45e96cf
data20 <data20@DESKTOP-6LBSFJP>
2021-03-17 11:43:01 +0900

    실습 3/17 2번째

---
+++ scn/src/scn2.java
@@ -0,0 +1,25 @@
+
+public class scn2 {
+
+	public static void main(String[] args) {
+
+		char a,b,c,d,e;
+		
+		a ='A'; //65
+		System.out.println( a+"의 숫자값=  "+ (int)a);
+		
+		b ='a'; //97
+		c=(char)(b+1);
+		System.out.println( b+ "의 숫자값=  "+ (int)b);
+		System.out.println( c+ "의 숫자값=  "+ (int)c);
+		
+		d=90;
+		e=(char)( d + 1);
+		System.out.println( d+ "의 숫자값=  "+(int)d);
+		System.out.println( e+ "의 숫자값=  "+(int)e);
+		
+		
+		
+	}
+
+}
