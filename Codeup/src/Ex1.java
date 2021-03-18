0d843e73d7dfd7828027dc7ad76eb3fb6cc2f0db
data20 <data20@DESKTOP-6LBSFJP>
2021-03-18 19:14:33 +0900

    code up 1001~1005번 (3/18)

--- Codeup/src/Ex1.java
+++ Codeup/src/Ex1.java
@@ -11,13 +11,10 @@ public class Ex1 {
 	System.out.printf("x*y="+x*y+"\n");
 	
 	System.out.println("귀찮당. 역시 ln이 더 좋아.");
-		
-		
-		
-		
-	
-
-	
+	System.out.println("Hello world");
+	System.out.println("Hello\nworld");
+	System.out.println("'Hello'");
+	System.out.printf("\"Hello World\"");
 	
 	
 	
