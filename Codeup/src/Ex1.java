1e3c7cc5df4d3a14a1c25edeedb5cb378c842b01
data20 <data20@DESKTOP-6LBSFJP>
2021-03-18 18:49:41 +0900

    3/18일

---
+++ Codeup/src/Ex1.java
@@ -0,0 +1,26 @@
+
+
+public class Ex1 {
+	
+
+	public static void main(String[] args) {
+	int x =100;
+	int y =50;
+	System.out.printf("Hello\nWorld");	// \n은 줄바꿈 이다.
+	
+	System.out.printf("x*y="+x*y+"\n");
+	
+	System.out.println("귀찮당. 역시 ln이 더 좋아.");
+		
+		
+		
+		
+	
+
+	
+	
+	
+	
+	}
+	
+}
