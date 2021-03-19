8125fe6a1d92168dfc45df5a6b47d32fc8b4440a
data20 <data20@DESKTOP-6LBSFJP>
2021-03-19 17:22:36 +0900

    code up 1006~1017 3/19일자

---
+++ Codeup/src/Ex2.java
@@ -0,0 +1,29 @@
+//import java.util.Scanner;
+public class Ex2 {
+
+	public static void main(String[] args) {
+		System.out.println("!@#$%^&*()1");
+		System.out.printf("\"C:\\Download\\hello.cpp\"");
+		System.out.println("ㅂ");
+		System.out.println();
+		int a;
+		a=15;
+		System.out.println(a);
+		char x = 'a';
+		System.out.println(x);
+		float b=3;
+		System.out.println((double)b);
+//	Scanner scn = new Scanner(System.in);
+
+//	int o =scn.nextInt();
+//		System.out.println(1+" "+2);	
+//			내가 출력해야 하는 값은 1.53 이다. math.random 뒤에 그냥 나누기 100을 하면 
+		double d=1.529025;
+	double shortd=Math.round(1.529025*100)/100.0; //1529.025
+	System.out.println(shortd);
+	int z = 10;
+	System.out.println(z+" "+z+" "+z);
+	}
+		
+
+}
