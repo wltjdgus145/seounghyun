785e57c1fcac2351a2ee67d056f0b17c9b40f0a8
data20 <wltjdgussla55555@gmail.com>
2021-03-22 18:07:12 +0900

    3/22 codeup~ 1023까지

---
+++ Codeup/src/Ex3.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Ex3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+//		int a,b,y,m,d,e;
+//		
+//		a=100;
+//		System.out.println("a의 값을 입력하시오==>>");
+//		a=scn.nextInt();
+//		System.out.println("정답입니다.");
+//		
+//		b=scn.nextInt();
+//		
+//		System.out.println("정답입니다.");
+//		
+//		System.out.println("");
+//		e=2021;
+//		m=03;
+//		d=22;
+//		y =scn.nextInt();
+//		m =scn.nextInt();
+//		d =scn.nextInt();
+//	System.out.println(e+"."+m+"."+d);
+		String a;
+		a="information";
+		
+		a=scn.next();
+		System.out.println("020718");
+		System.out.println("information");
+		
+		String p;
+		p ="BOY";
+//			for(p=0; p<=3; p++) {
+				System.out.println('B');
+				System.out.println('O');
+				System.out.println('Y');
+				
+				
+			System.out.println(10.0/3);
+				
+		
+		
+	}
+
+}
