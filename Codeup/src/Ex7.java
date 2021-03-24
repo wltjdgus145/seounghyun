8dc605f0e86ecad1d22ce69893fc3209b4b1406a
data20 <wltjdgussla55555@gmail.com>
2021-03-24 16:55:47 +0900

    3/24 codeup ++

---
+++ Codeup/src/Ex7.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Ex7 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		int a;
+		
+		a=scn.nextInt();
+		
+		if(a>=90 && a<=100) {
+			if(a<=95) {
+				System.out.println("A- 입니다.");
+				
+			}
+			else {
+				
+				System.out.println("A+ 입니다. 축하드립니다.");
+				
+			}
+			
+			
+		}
+			
+			
+		
+	if(a>=70 && a<90 ) {
+		
+		System.out.println("B 입니다.");
+		
+	}
+	
+	
+	}
+
+}
