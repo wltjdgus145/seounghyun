90e91c13e62269b5e15ed046d466a432b7aa1a02
data20 <wltjdgussla55555@gmail.com>
2021-03-24 16:54:31 +0900

    3/24 code up

---
+++ Codeup/src/Ex5.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Ex5 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		int a=5 ,b=4;
+		
+		if(a<b) {
+			System.out.println(1);
+			
+			
+		}else {
+			System.out.println(0);
+			
+			
+		a =scn.nextInt();	
+		if( a==1 ) {
+			System.out.println("false");	
+			
+			
+		}else if(a==0) {
+			System.out.println("true");
+			
+	}else {
+			System.out.println("gimozzi");	
+			
+		}		
+
+//		int c=90;
+	 	
+		int x =1;
+
+		System.out.println(~x);
+	
+	}
+		
+		
+
+		
+		
+	}
+
+}
