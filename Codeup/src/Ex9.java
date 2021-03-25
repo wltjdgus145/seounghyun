a40a842bedb41acd24cfbf03bb43f240aeefa155
data20 <wltjdgussla55555@gmail.com>
2021-03-25 20:02:38 +0900

    3/25 code up ~~ 1080 까지

---
+++ Codeup/src/Ex9.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex9 {
+
+	public static void main(String[] args) {
+		
+		int a;
+	Scanner scn = new Scanner(System.in);
+	a=scn.nextInt();
+	while(a--!=0)  {
+		
+		System.out.println(8);
+		
+		int sum,i;
+		
+		
+		
+		
+		
+	}	
+    	
+    
+	}
+
+}
