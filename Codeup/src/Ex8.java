eb3757e7329a6a550558d8671eef38fea2c4e613
data20 <wltjdgussla55555@gmail.com>
2021-03-25 19:59:58 +0900

    3/25 code up ~~ 1080 까지

---
+++ Codeup/src/Ex8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Ex8 {
+
+	public static void main(String[] args) {
+
+		int A,B,C,D;
+		System.out.println("얼마나 잘했을까요?? ==>>");	
+		
+	Scanner scn = new Scanner(System.in);
+
+	A =scn.nextInt();
+	
+	switch(A) {
+		
+	case 'A' :
+		System.out.println("Best!!");
+	
+			break;
+			
+	case 'B' :
+		System.out.println("good!");
+		break;
+	case 'C' : 
+		System.out.println("run!");
+		break;
+	case 'D':
+			System.out.println("slowly~");
+	default:
+		System.out.println("what?");
+		
+	}
+  }
+
+}
