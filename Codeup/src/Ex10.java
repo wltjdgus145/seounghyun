2e7756e308b6929285f63df02bacf6f4f0d96322
data20 <wltjdgussla55555@gmail.com>
2021-03-26 16:50:06 +0900

    3/26 code up

---
+++ Codeup/src/Ex10.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Ex10 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+//		char x = 'a';
+//		
+//		do {
+//			System.out.println("a를 입력하시면 알파벳 순서대로 나옵니다.>");
+//			int a =scn.nextInt();
+//		
+//		
+//	
+//		}	while(x !='z') {
+//			System.out.println(x);
+//			
+//			x ++;  x ++ = x=x+1 x +=1
+	
+		int i =5;
+		while(i --!=0) {
+			System.out.println(i+1);
+			
+			System.out.println("다이ㅋ");
+		}
+		
+		
+		
+		
+			}
+		
+		
+	
+
+}
