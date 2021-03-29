598bcc46ce41ad7cc5674edc079808d439a11a6a
data20 <wltjdgussla55555@gmail.com>
2021-03-29 16:55:59 +0900

    3/29

---
+++ scn/src/scn/lwe.java
@@ -0,0 +1,19 @@
+package scn;
+import java.util.Scanner;
+public class lwe {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+	int a =1000;
+	
+		if(a>=100) {
+		System.out.println("나는 집을 갈거양");	
+			
+			
+			
+			
+		}
+	}
+
+}
