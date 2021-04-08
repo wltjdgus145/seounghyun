ab53b3e8ac5b46003e2f2181fef0f1a84075130a
data20 <wltjdgussla55555@gmail.com>
2021-04-08 20:57:26 +0900

    04/05 코딩

--- SeoungHyun/src/rwrw.java
+++ SeoungHyun/src/rwrw.java
@@ -3,9 +3,22 @@ public class rwrw {
 
 	public static void main(String[] args) {
 		
-		String a = "오늘은 정말이지 힘들고 지치고 하기싫고 짜증나고 배고프고 미치겠는 날이군요.";
+		//switch 문으로 주사위 개수 출력하기.
 		
-		System.out.println(a);
+		
+		
+		for(int i =1; i<=6; i++) {
+			
+			for(int j =1; j<=6; j++) {
+				
+				System.out.println(i+" "+j);
+			}
+			
+		}
+		
+		
+
+		
 		
 		
 	}
